842cd8f416ec4d758d445809db8eca78ba4a7d35
TomerDun <tomer@dunsky.name>
2025-07-24 15:08:38 +0300

    improved render function

--- Module 3 - JS and Web/tweeter/render.js
+++ Module 3 - JS and Web/tweeter/render.js
@@ -18,6 +18,13 @@ export class Renderer {
             postContent.className = 'post-content';
             postContent.innerText = post.text;
             postDiv.appendChild(postContent);
+
+            // Delete button
+            const postDeleteButton = document.createElement('button');
+            postDeleteButton.className = 'post-delete-button';
+            postDeleteButton.setAttribute('data-id', post.id);
+            postDeleteButton.innerText = 'Delete';
+            postDiv.appendChild(postDeleteButton);
             
             // comments
             const commentsContaier = document.createElement('div');
