c88aafc85b7d8bbeff9614dd84c7fafcbc2a02ae
TomerDun <tomer@dunsky.name>
2025-08-10 14:03:26 +0300

    Finished Ex 2

---
+++ Module 4 - React/data flow/src/ex2/Ex2.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import List from "./List";
+import Conversation from "./Conversation";
+
+export default function Ex2() {
+    const [conversations, setConversations] = useState(
+        [
+            {
+                with: "Laura", convo: [
+                    { text: "Hi", sender: "self" },
+                    { text: "You there?", sender: "self" },
+                    { text: "Yeah, hi, what's up?", sender: "other" }
+                ]
+            },
+            {
+                with: "Dad", convo: [
+                    { text: "Have you finished your school work yet?", sender: "other" },
+                    { text: "Yes.", sender: "self" },
+                    { text: "What do you mean, yes?", sender: "other" },
+                    { text: "??", sender: "self" }
+                ]
+            },
+            {
+                with: "Shoobert", convo: [
+                    { text: "Shoobert!!!", sender: "self" },
+                    { text: "Dude!!!!!!!!", sender: "other" },
+                    { text: "Shooooooooo BERT!", sender: "self" },
+                    { text: "You're my best friend", sender: "other" },
+                    { text: "No, *you're* my best friend", sender: "self" },
+                ]
+            }
+        ]
+    )
+    const [displayConversation, setDisplayConversation] = useState(null); 
+
+    return (
+        displayConversation === null ? <List /> : <Conversation />
+    )
+}
