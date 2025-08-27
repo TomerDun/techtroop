c820a8fbee0643e296f9bed60f7ac89062c10627
TomerDun <tomer@dunsky.name>
2025-08-27 15:59:52 +0300

    Finished Ex5

--- Module 5 - Server/Express/CRUD/server.js
+++ Module 5 - Server/Express/CRUD/server.js
@@ -33,6 +33,41 @@ app.post('/word', (req, res) => {
     res.status(201).json(output);
 })
 
+app.delete('/word', (req, res) => {
+    const word = req.query.word;
+    if (word in wordCounter) {
+        delete wordCounter[word];
+        res.status(203).send({msg: 'Word Deleted'});
+    }
+    else {
+        res.status(400).send({msg: 'Word not found'});
+    }
+})
+
+app.post('/sentence', (req, res) => {
+    let newWords = 0;
+    let oldWords = 0;
+
+    const words = req.body.sentence.split(' ');
+    for (const word of words) {
+        if (word in wordCounter) {
+            oldWords ++;
+            wordCounter[word] ++;
+        }
+        else {
+            newWords ++;
+            wordCounter[word] = 1;
+        }
+    }
+
+    const output = {
+        text: `Added ${newWords} New words and updated ${oldWords} Existing words`,
+        currentCount: -1
+    }
+
+    res.status(201).json(output);
+})
+
 app.listen(8080, (req, res) => {
     console.log('Server is running on port 8080');
 })
