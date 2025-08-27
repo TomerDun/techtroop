b0a19df02b41ddf33e88a1746145bd438fd140fb
TomerDun <tomer@dunsky.name>
2025-08-27 15:41:43 +0300

    Finished Ex3

--- Module 5 - Server/Express/CRUD/server.js
+++ Module 5 - Server/Express/CRUD/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { wordCounter } from './models/wordCounter.js';
+
 
 const app = express();
 
@@ -8,6 +10,30 @@ app.get('/sanity', (req, res) => {
     res.send('Server is running!')
 })
 
+app.get('/count', (req, res) => {
+    if (req.query.word && req.query.word in wordCounter) {
+        res.status(200).json({count: wordCounter[req.query.word]})
+    }
+    else {
+        res.status(400).json({count: 0});
+    }
+})
+
+app.post('/word', (req, res) => {
+    const newWord = req.body.word;
+    if (newWord in wordCounter) {
+        wordCounter[newWord] ++;
+    }
+    else {wordCounter[newWord] = 1};
+
+    const output = {
+        text: 'added ' + newWord,
+        currentCount: wordCounter[newWord],
+    }
+    res.status(201).json(output);
+})
+
 app.listen(8080, (req, res) => {
     console.log('Server is running on port 8080');
 })
+
