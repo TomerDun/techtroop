8c567e913cd4c9ac219aa7815cf1d65ff04dd996
TomerDun <tomer@dunsky.name>
2025-08-28 14:53:43 +0300

    Added rate limiter

--- Module 5 - Server/Express/Validations/Ex3/server.js
+++ Module 5 - Server/Express/Validations/Ex3/server.js
@@ -2,10 +2,19 @@ import express from 'express';
 import { postsRouter } from './routes/postsRouter.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { reqLogger, resFormatter } from './middleware/generalFormatter.js';
+import rateLimit from 'express-rate-limit'
 
 const app = express();
 
+const rateLimiter = rateLimit({
+    windowMs: 1000 * 60,
+    max: 2,
+    message: 'Rate limit passed.'
+
+})
+
 app.use(express.json());
+app.use(rateLimiter);
 app.use(reqLogger);
 // app.use(resFormatter)
 
