174a9558570bc4d0acbc31a5f3e61cd96bcd0702
TomerDun <tomer@dunsky.name>
2025-09-01 16:47:48 +0300

    Finished Ex3

--- Module 6 - Database/PokeCorp/pokeStats.js
+++ Module 6 - Database/PokeCorp/pokeStats.js
@@ -7,4 +7,16 @@ async function getHeavy() {
     console.log(output);        
 }
 
-getHeavy();
+async function findByType(typeName) {
+    let [res] = await sequelize.query('SELECT id FROM pokemon_types WHERE name = :typeName', {replacements: {typeName: typeName}});
+    const typeId = res[0].id;
+
+    const query = `SELECT name FROM pokemons WHERE type_id = :typeId`;
+    [res] = await sequelize.query(query, {replacements: {typeId}});
+    console.log(res);
+    
+}
+
+// await getHeavy();
+await findByType('grass');
+await findByType('fire');
